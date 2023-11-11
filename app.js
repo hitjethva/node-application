6b8cb83be472b210e51002696959b75bc8e2be5b
hitjethva@gmail.com <hitjethva@gmail.com>
2023-11-11 10:05:30 +0530

    Modify Node App

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Node Hello World Application!\n');
+  res.send('Node Hello World Application Updated!\n');
 });
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
